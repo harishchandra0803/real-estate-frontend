3a0818048a63602b75ea1aed38a5de43fc958ab2
harishchandra0803 <100511916+harishchandra0803@users.noreply.github.com>
2025-02-12 23:02:04 +0530

    Update UpdateListing.jsx

--- src/pages/UpdateListing.jsx
+++ src/pages/UpdateListing.jsx
@@ -62,7 +62,7 @@ export default function CreateListing() {
           setFormData({
             ...formData,
             imageUrls: formData.imageUrls.concat(urls),
-          
+            });
           setImageUploadError(false);
           setUploading(false);
         })
